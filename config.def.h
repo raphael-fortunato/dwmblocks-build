8ad3102ad6bfcf8a160567708d47c919666a0de8
4170512 <r.furtunato@students.uu.nl>
2021-09-26 22:10:51 +0200

    config

--- config.def.h
+++ config.def.h
@@ -43,9 +43,11 @@ static const char delimiter[] = { ' ', ' ', ' ', DELIMITERENDCHAR };
 
 static Block blocks[] = {
 /*      pathu                           pathc                           interval        signal */
-        { PATH("calendar.sh"),          NULL,                           30,             1},
-        { PATH("volume.sh"),            PATH("volume_button.sh"),       0,              2},
-        { PATH("cpu_temp.sh"),          PATH("cpu_temp_button.sh"),     1,              4},
-        { PATH("battery.sh"),           PATH("battery_button.sh"),      30,             3},
-        { NULL } /* just to mark the end of the array */
+        { PATH("sb_timedate"),          NULL,                           30,             1},
+        { PATH("sb_battery"),            PATH("volume_button.sh"),       0,              2},
+        { PATH("sb_cpu"),          PATH("cpu_temp_button.sh"),     1,              4},
+        { PATH("sb_disk"),           PATH("battery_button.sh"),      30,             3},
+        { PATH("sb_memory"),           PATH("battery_button.sh"),      30,             3},
+        { PATH("sb_forecast"),           PATH("battery_button.sh"),      30,             3},
+        { PATH("sb_spotify"),           PATH("battery_button.sh"),      30,             3},
 };
